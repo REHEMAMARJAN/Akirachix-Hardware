76274cc0590c322818f2076bde087d5aa41b1222
Rehemamarjan <rehemamarjan50@gmail.com>
2018-02-19 16:00:59 +0300

    added thinkingprocess.cpp file

---
+++ thinkingprocess.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int a;
+ int b;
+cout <<"enter first number";
+cin >> a;
+cout <<"entersecond number";
+cin >> b;
+ if (a>b){
+ 	cout<< "1st number is greater than second number";
+
+
+ }
+ 	
+ else if(a==b) 
+
+ {
+ 	cout<<"first number is equal to second";
+
+ 	
+ }
+
+else {
+	cout<<"second is greater than first";
+} 
+    return 0;
+}
+
+
